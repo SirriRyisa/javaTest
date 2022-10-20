97e7d871ac03a3f7c483d2b3e312eda76f5ba01d
SirriRyisa <sirriryisa@gmail.com>
2022-10-20 03:40:27 +0100

    Added switch cases to determine a day of the week

---
+++ SwitchCase.java
@@ -0,0 +1,19 @@
+public class SwitchCase{
+  //This class will have a dynmic method that can make use of class instances.
+  
+  public static void main(String[] args){
+    int day = 7;
+
+    switch(day) {
+      case 1:
+      System.out.println("I like monday");
+      break;
+      case 2:
+      System.out.println("Don't get me wrong, saturdays are my best days");
+      break;
+      case 7:
+      System.out.println("Yeeeeee!!! Sundays are for famlilies");
+      break;
+    }
+  }
+}
