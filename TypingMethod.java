1c0140b7543a9258f554f63e6909f0ee8bf383e8
SirriRyisa <sirriryisa@gmail.com>
2022-10-24 16:21:45 +0100

    Modified mmethods

--- TypingMethod.java
+++ TypingMethod.java
@@ -1,6 +1,31 @@
 public class TypingMethod {
   // A class is simply a blueprint of an object
   public static void main(String[] args) {
+int num=1;
+int nums=0; 
+while(num<1001) 
+ { 
+ nums=nums+num; 
+ num++; 
+ }  
+System.out.println(nums);
+
+// End of static method for while loop
+
+int start = 12;
+int end = 103;
+
+for(int i = start; i <=end; i++){
+if( i % 2 == 0 ){
+System.out.println("even num "+i);
+}
+}
+
+// End of static method to find sum of numbers btn 12-103
+
+
+// Dynamic methods for days of week and condition
+
     new TypingMethod().switchcase("Sunday", 7);
     new TypingMethod().condition("Sirri");
     
